1a74dfab5a0a02382cbcc1230c05c3588c735b83
Tyuiu.BrukhovAA@outlook.com <bruhov_uch@mail.ru>
2023-11-15 18:59:32 +0500

    Add project files.

---
+++ Tyuiu.BrukhovAA.Sprint5.Task0.V30.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using System.IO;
+using Tyuiu.BrukhovAA.Sprint5.Task0.V30.Lib;
+
+namespace Tyuiu.BrukhovAA.Sprint5.Task0.V30.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidSaveToFileTextData()
+        {
+            string path = @"C:\Users\bruho\source\repos\Tyuiu.BrukhovAA.Sprint5\Tyuiu.BrukhovAA.Sprint5.Task0.V30\bin\Debug\OutPutFileTask0.txt";
+
+
+            FileInfo fl = new FileInfo(path);
+            bool fileExists = fl.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.BrukhovAA.Sprint5.Task0.V30.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BrukhovAA.Sprint5.Task0.V30.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BrukhovAA.Sprint5.Task0.V30.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("31d7e3e1-366a-4d0b-9af0-5ffe1158be1e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
