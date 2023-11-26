663d62a70d953e1c9082d0e7f0cac414ee0872c6
Tyuiu.BrukhovAA@outlook.com <bruhov_uch@mail.ru>
2023-11-26 19:30:08 +0500

    Task6

---
+++ Tyuiu.BrukhovAA.Sprint5.Task6.V1.Test/DataServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using System.IO;
+using Tyuiu.BrukhovAA.Sprint5.Task6.V1.Lib;
+
+namespace Tyuiu.BrukhovAA.Sprint5.Task6.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckFile()
+        {
+            string path = @"C:\DataSprint5\Sprint5Task6\InPutDataFileTask6V1.txt";
+
+
+            FileInfo fl = new FileInfo(path);
+            bool fileExists = fl.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint5\Sprint5Task6\InPutDataFileTask6V1.txt";
+            int res = ds.LoadFromDataFile(path);
+            int wait = 6;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BrukhovAA.Sprint5.Task6.V1.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BrukhovAA.Sprint5.Task6.V1.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BrukhovAA.Sprint5.Task6.V1.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8065cf40-0bd3-4a33-8d9c-91306cf698c4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
